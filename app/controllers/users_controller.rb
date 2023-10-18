061727f954ab90ebd8ea7a3624fb6ea770f831e3
PikkaStar <yokotaka1226@gmail.com>
2023-10-18 01:49:31 +0000

    user show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_many :books,dependent: :destroy
+         has_one_attached :profile_image
+
+         def get_profile_image(width,height)
+           if profile_image.attached?
+             profile_image
+           else
+             'no_image'
+           end
+
+         end
+
 end
