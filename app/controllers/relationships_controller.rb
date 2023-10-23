3931ddba15b12ef74ad840f7827c5b4a13fa7111
PikkaStar <yokotaka1226@gmail.com>
2023-10-23 08:27:58 +0000

    follow_finish

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,23 @@
 class RelationshipsController < ApplicationController
 
   def create
-    following = current_user.relationships.build(follower_id: params[:user_id])
-    following.save
+    current_user.follow(params[:user_id])
     redirect_to request.referer
   end
 
   def destroy
-    following = current_user.relationships.find_by(follower_id: params[:user_id])
-    following.destroy
+    current_user.unfollow(params[:user_id])
     redirect_to request.referer
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+  
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
          has_many :books,dependent: :destroy
          has_many :book_comments,dependent: :destroy
          has_many :favorites,dependent: :destroy
-         
-         has_many :relationships,foreign_key: "followed_id",dependent: :destroy
-         has_many :reverse_of_relationships,class_name: 'Relationship',foreign_key: 'follower_id',dependent: :destroy
-         
-         has_many :followings,through: :relationships,source: :follower
-         has_many :follower,through: :reverse_of_relationships,source: :following
-         
+
+         has_many :relationships,class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
+         has_many :reverse_of_relationships,class_name: 'Relationship',foreign_key: 'followed_id',dependent: :destroy
+
+         has_many :followings,through: :relationships,source: :followed
+         has_many :followers,through: :reverse_of_relationships,source: :follower
+
          has_one_attached :profile_image
          validates :name,uniqueness: true,length: {in: 2..20}
          validates :introduction,length: {maximum: 200}
@@ -23,7 +23,19 @@ class User < ApplicationRecord
            else
              'no_image'
            end
-
          end
+         
+         def follow(user_id)
+           relationships.create(followed_id: user_id)
+         end
+         
+         def unfollow(user_id)
+           relationships.find_by(followed_id: user_id).destroy
+         end
+         
+         def following?(user)
+           followings.include?(user)
+         end
+
 
 end
