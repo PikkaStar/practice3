e1cdecf6c4bc74670307164447e4729c404696c6
PikkaStar <yokotaka1226@gmail.com>
2023-10-23 11:53:19 +0000

    finish_relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
          has_many :favorites,dependent: :destroy
 
          has_many :relationships,class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
-         has_many :reverse_of_relationships,class_name: 'Relationship',foreign_key: 'followed_id',dependent: :destroy
-
          has_many :followings,through: :relationships,source: :followed
+         
+         has_many :reverse_of_relationships,class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
          has_many :followers,through: :reverse_of_relationships,source: :follower
-
+         
          has_one_attached :profile_image
          validates :name,uniqueness: true,length: {in: 2..20}
          validates :introduction,length: {maximum: 200}
@@ -24,18 +24,17 @@ class User < ApplicationRecord
              'no_image'
            end
          end
-         
-         def follow(user_id)
-           relationships.create(followed_id: user_id)
-         end
-         
-         def unfollow(user_id)
-           relationships.find_by(followed_id: user_id).destroy
-         end
-         
-         def following?(user)
-           followings.include?(user)
-         end
-
+      
+      def follow(user_id)
+        relationships.create(followed_id: user_id)
+      end
+      
+      def unfollow(user_id)
+        relationships.find_by(followed_id: user_id).destroy
+      end
+      
+      def following?(user)
+        followings.include?(user)
+      end
 
 end
